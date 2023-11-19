f112e377186c1770793f2dc6cc690cda5e8f2c7e
matt.a@myplanet.com <matt.a@myplanet.com>
2023-11-18 23:42:47 -0500

    Stream Chat Demo

---
+++ ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ChatDemo
+//
+//  Created by Matt Ahmad on 2023-11-18.
+//
+import StreamChat
+import StreamChatUI
+import UIKit
+
+class DemoChannelList: ChatChannelListVC {}
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
